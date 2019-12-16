b7edcbc97169da20b1978579049e4fcad9a80ef1
Esteve Millán <hello@stvmillan.com>
2019-12-16 17:36:37 +0100

    base setup

--- serverServices/config.js
+++ serverServices/config.js
@@ -3,8 +3,9 @@ import infoSource from '../utils/infoSource';
 // Config API.
 export default {
   authRequire: true,
-  baseUrl: process.env.NODE_ENV !== 'production' ? 'http://localhost:3001/api' : infoSource.baseUrl,
+  // baseUrl: process.env.NODE_ENV !== 'production' ? 'http://localhost:3001/api' : infoSource.baseUrl,
+  baseUrl: 'http://ec2-3-122-75-193.eu-central-1.compute.amazonaws.com:4000/api',
   cookiesCart: 604800, // 7 días
   cookiesExpires: 2592000, // 30 días
-  urlImages: infoSource.urlImages
+  urlImages: 'https://s3-eu-central-1.amazonaws.com/homoludicus/'
 };
