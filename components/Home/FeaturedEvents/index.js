9d936690ed0e734a4547777ee1cae90c8e0e59c7
Gemma Barrera <lagemyo@gmail.com>
2020-01-10 09:19:09 +0100

    added title to Events image

--- components/Home/FeaturedEvents/index.js
+++ components/Home/FeaturedEvents/index.js
@@ -2,27 +2,27 @@ import React from 'react';
 
 const FeaturedEvents = () => {
   return (
-      <div className="featured-events">
-          <img src="/images/event.jpeg" alt="event" />
-          <div className="event-info">
-              <h3 className="event-title">
-                    Torneig Warhammer 2019
-                </h3>
-              <div className="event-desc">
-                    Lorem ipsum dolor sit amet consectetur adipiscing,
-                    elit ullamcorper himenaeos eros fermentum.
-                    Sollicitudin tellus laoreet faucibus tortor.
-                </div>
-              <div className="event-contact-share">
-                  <div className="event-contact">
-                      <button className="button button-yellow">Apunta't</button>
-                    </div>
-                  <div className="event-share">
-                      <img src="/icon/icon-share-white.svg" alt="share" />
-                    </div>
-                </div>
-            </div>
+    <div className="featured-events">
+      <h2 className="image-title">Pròxims esdeveniments</h2>
+      <img src="/images/event.jpeg" alt="event" />
+      <div className="event-info">
+        <h3 className="event-title">
+          Torneig Warhammer 2019
+        </h3>
+        <div className="event-desc">
+          Lorem ipsum dolor sit amet consectetur adipiscing,
+          elit ullamcorper himenaeos eros fermentum.
         </div>
+        <div className="event-contact-share">
+          <div className="event-contact">
+            <button className="button button-yellow">Apunta't</button>
+          </div>
+          <div className="event-share">
+            <img src="/icon/icon-share-white.svg" alt="share" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
