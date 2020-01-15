6d711cbfbd23e11bc859b782731bfa6cb1719893
Gemma Barrera <lagemyo@gmail.com>
2020-01-15 10:03:26 +0100

    finished form

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { TableBody } from '@material-ui/core';
+import useForm from '@bit/iamtechnologies.iamtech-js.use-form';
 import Layout from '../../components/Layout';
 
 const aboutUs = () => {
   const team = ['Corey Hale', 'Evan Boyd', 'Randy Frazier'];
+
+  const defaultValues = {
+    name: '',
+    email: '',
+    matter: '',
+    message: ''
+  };
+
+  const { values, useInput, isValid, errors, showErrors } = useForm(defaultValues);
+
+  const ERRORS_TEXT = {
+    isRequired: 'Campo obligatorio',
+    isEmail: 'Formato de email inválido'
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('values', values);
+  };
+
   return (
     <Layout>
       <section className="about-us-sec">
@@ -34,15 +54,31 @@ const aboutUs = () => {
           </div>
           <div className="contact-column">
             <h4>Contacta'ns</h4>
-            <form>
-              <input type="text" name="name" value="Escriu el teu nom" />
-              <input type="email" value="El teu email" />
-              <input type="text" value="Asumpte" />
-              <input type="text" className="message" value="Escriu el teu missatge" />
+            <form onSubmit={handleSubmit}>
+              <input type="text" {...useInput('name', 'isRequired')} placeholder="Escriu el teu nom" />
+              <div className="form-errors">
+                {showErrors.name && errors.name && errors.name.map(err => <div key={err} className="form-errors-item">{ERRORS_TEXT[err]}</div>)}
+              </div>
+              <input type="text" {...useInput('email', 'isEmail,isRequired')} placeholder="El teu email" />
+              <div className="form-errors">
+                {showErrors.email && errors.email && errors.email.map(err => <div key={err} className="form-errors-item">{ERRORS_TEXT[err]}</div>)}
+              </div>
+              <input type="text" {...useInput('matter', 'isRequired')} placeholder="Asumpte" />
+              <div className="form-errors">
+                {showErrors.matter && errors.matter && errors.matter.map(err => <div key={err} className="form-errors-item">{ERRORS_TEXT[err]}</div>)}
+              </div>
+              <input type="text" className="message" {...useInput('message', 'isRequired')} placeholder="Escriu el teu missatge" />
+              <div className="form-errors">
+                {showErrors.message && errors.message && errors.message.map(err => <div key={err} className="form-errors-item">{ERRORS_TEXT[err]}</div>)}
+              </div>
               <div className="button-div">
-                <button type="submit" className="button-yellow">
-                  Enviar
-                </button>
+                {isValid && (
+                  <button
+                    type="submit"
+                    className="button-yellow"
+                  >Enviar
+                  </button>
+                )}
               </div>
             </form>
           </div>
