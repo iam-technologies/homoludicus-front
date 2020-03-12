e937b8140aecbbf398c2491936c1f4905b761df7
Gemma Barrera <lagemyo@gmail.com>
2020-03-12 18:41:28 +0100

    added fixed layer to the dropdown menu

--- components/Navbar/Mobile/MobileMenu/index.js
+++ components/Navbar/Mobile/MobileMenu/index.js
@@ -5,16 +5,18 @@ const MobileMenu = ({ showMenu, mainMenu }) => {
   const menuState = showMenu ? '-open' : '';
 
   return (
-    <div className={`mobile-menu${menuState}`}>
-      {mainMenu.map((item) => {
-        return (
-          <Link key={item.title} href={item.url}>
-            <a>
-              <p>{item.title}</p>
-            </a>
-          </Link>
-        );
-      })}
+    <div className="mobile-menu-div">
+      <div className={`mobile-menu${menuState}`}>
+        {mainMenu.map((item) => {
+          return (
+            <Link key={item.title} href={item.url}>
+              <a>
+                <p>{item.title}</p>
+              </a>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
