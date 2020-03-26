5f2b43600c2ebb6b652c54e942a5b87cd9345526
Gemma Barrera <lagemyo@gmail.com>
2020-03-26 13:47:58 +0100

    added layout and section-header to 'serveis' page

--- pages/serveis.js
+++ pages/serveis.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Layout from '../components/Layout';
+import SectionHeader from '../components/SectionHeader';
 
 const ServicesPage = () => {
-  return <p>I'm the Services page</p>;
+  const title = 'Serveis';
+  const text = '';
+  return (
+    <Layout>
+      <SectionHeader title={title} text={text} />
+      <div style={{ height: '600px' }}>Próximamente...</div>
+    </Layout>
+  );
 };
 
 export default ServicesPage;
